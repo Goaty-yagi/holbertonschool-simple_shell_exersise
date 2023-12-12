1945f5143e7e82103f46a07341facc460286e663
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-12 13:55:58 +1100

    refactor

--- Arguments/main.c
+++ Arguments/main.c
@@ -6,22 +6,48 @@
  * Return: Always 0.
  */
 
-int main(void)
+int main(int ac, char **av)
 {
-    // int length;
-    // length = read_line();
-    // printf("printed length:%d\n", length);
+    /*
+    *** START 0-print_av function ***
+
+    (void)ac;
+    print_av(av);
+
+    *** END 0-print_av function ***
+    */
+
+
+    /*
+    *** START 1-read_line function
+
+    (void)ac;
+    (void)av;
+    int length;
+    length = read_line();
+    printf("printed length:%d\n", length);
+
+    *** END 1-read_line function
+    */
+
+
+    // *** START 2-command_line_to_av.c
 
     char **ar;
     int counter;
+
+    (void)ac;
+    (void)av;
 
     counter = 0;
     ar = command_line_to_av();
     while (*ar)
     {
-        printf("%d:%s\n",counter, *ar);
+        printf("%d:%s\n", counter, *ar);
         counter = counter + 1;
         ar++;
     }
+    // *** END 2-command_line_to_av.c
+
     return (0);
 }
