92b7e6b6bcf7fbbba11cc63a33b25d10946623e5
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-14 16:52:09 +1100

    init commit

---
+++ env_prototype/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(int ac, char **av)
+{
+    char **ap;
+
+    (void)ac;
+    (void)ap;
+    (void)av;
+    // file_info_ex(ac, av);
+    file_info_exercise(ac, av);
+    // opendir_readdir();
+    // printf("%s", ap[8]);
+    // ap = get_dir(".");
+    return (0);
+}
---
+++ env_prototype/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+
+#endif
